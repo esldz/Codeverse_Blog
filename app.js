2cf98c4ec88ae6e7560d176ba3875e84e4e37f71
esmanur yıldız <101506133+esldz@users.noreply.github.com>
2023-08-28 22:31:37 +0300

    Update app.js add mongoose

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require('mongoose');
 
 const aboutContent = "While learning new information about the software, I will share the information I learned on this software blog.";
 
@@ -12,67 +13,71 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-var posts=[];
+mongoose.connect('mongodb://localhost:27017/BlogDB', { useNewUrlParser: true });
 
-app.get("/",(req,res)=>{
-  res.render("home.ejs", { posts : posts });
+const postSchema = new mongoose.Schema({
+  title: String,
+  text: String
 });
 
-app.get("/about",(req,res)=>{
-  res.render("about.ejs",{ aboutContent : aboutContent});
+const Post = mongoose.model('Post', postSchema);
+
+app.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.render("home.ejs", { posts: posts });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Veritabanı hatası");
+  }
 });
 
-app.get("/contact",(req,res)=>{
+app.get("/about", (req, res) => {
+  res.render("about.ejs", { aboutContent: aboutContent });
+});
+
+app.get("/contact", (req, res) => {
   res.render("contact.ejs");
 });
 
-app.get("/compose",(req,res)=>{
+app.get("/compose", (req, res) => {
   res.render("compose.ejs");
 });
 
-app.post("/compose",(req,res)=>{
-  const post={
+app.post("/compose", async (req, res) => {
+  const post = new Post({
     title: req.body.postTitle,
-    text: req.body.postText,
-  };
+    text: req.body.postText
+  });
 
-  posts.push(post);
-  res.redirect("/");
+  try {
+    await post.save();
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Veritabanı hatası");
+  }
 });
 
-app.get("/posts/:postName",(req,res)=>{
-  const requestTitle=(req.params.postName);
+app.get("/posts/:postName", async (req, res) => {
+  const requestTitle = _.lowerCase(req.params.postName);
 
-  for(i=0;i<posts.length;i++){
-    if(_.lowerCase(posts[i].title)===_.lowerCase(requestTitle)){
-      res.render("post",{
-        title: posts[i].title,
-        text: posts[i].text,
+  try {
+    const post = await Post.findOne({ title: requestTitle });
+    if (post) {
+      res.render("post", {
+        title: post.title,
+        text: post.text
       });
-     }
-     else{
+    } else {
       res.status(404).render("pageNotFound.ejs");
-     }
-  };
-
-  // for(i=0;i<posts.length;i++){
-  //   if(_.lowerCase(posts[i].title)===_.lowerCase(requestTitle)){
-  //     console.log("Match Found");
-  //    }
-  //    else{
-  //     console.log("Not a match");
-  //    }
-  // };
-
-  // posts.forEach(function(post){
-  //   const storedTitle=post.title;
-
-  //   if(storedTitle===requestTitle){
-  //       console.log("Match Found!");
-  //   }
-  // });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Veritabanı hatası");
+  }
 });
 
-app.listen(3000, function() {
+app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
